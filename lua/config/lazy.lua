2986274911a676475754d32e8c0da11f9b84fd12
suiyuan <thetallestlc@qq.com>
2024-07-06 15:31:16 +0800

    在nvim禁用鼠标，添加语言插件

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,7 +11,7 @@ require("lazy").setup({
     spec = {
         -- add LazyVim and import its plugins
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-        { import = "lazyvim.plugins.extras.dap.core" },
+        -- { import = "lazyvim.plugins.extras.dap.core" },
         -- import/override with your plugins
         { import = "plugins" },
     },
