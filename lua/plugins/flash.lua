63e005ae1263f6508d8f82a89565ed81d1a93afd
suiyuan <thetallestlc@qq.com>
2024-07-08 14:50:03 +0800

    remove flash search backgroud color grey

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,9 @@
+return {
+    "folke/flash.nvim",
+    opts = {
+        highlight = {
+            -- backdrop = false, -- this doesn't do what I'd expect (i.e. remove the comment-colored backdrop)
+            groups = { backdrop = "" },
+        },
+    },
+}
